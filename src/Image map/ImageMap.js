41e552a16d841978ceda06ee66cc53150b97da5c
mokshteng <moksh.teng@byjus.com>
2023-03-01 17:17:08 +0530

    images added

---
+++ src/Image map/ImageMap.js
@@ -0,0 +1,10 @@
+import csk from "../assets/teamimages/csk.png";
+import dd from "../assets/teamimages/dd.png";
+import lsg from "../assets/teamimages/lsg.png";
+import rr from "../assets/teamimages/rr.png";
+import mi from "../assets/teamimages/mi.png";
+import rcb from "../assets/teamimages/rcb.png";
+
+const imagemap = { "csk": csk, "dd": dd, "lsg": lsg, "rr": rr, "mi": mi, "rcb": rcb };
+
+export default imagemap;
